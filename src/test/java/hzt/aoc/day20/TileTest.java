3f4bdfb890f1b7977d9269590faa37ba2401e513
HZUIDERV <hans.zuidervaart@sogeti.com>
2022-01-25 19:53:41 +0100

    - improved testorrientationsMethod

--- src/test/java/hzt/aoc/day20/TileTest.java
+++ src/test/java/hzt/aoc/day20/TileTest.java
@@ -1,25 +1,28 @@
 package hzt.aoc.day20;
 
+import hzt.collections.MutableListX;
+import hzt.strings.StringX;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TileTest {
 
     @Test
     void testOrientationsWithP() {
-        List<String> list = new ArrayList<>();
-        list.add("#### ");
-        list.add("#   #");
-        list.add("#### ");
-        list.add("#    ");
-        list.add("#    ");
+        var list = MutableListX.of(
+                "#### ",
+                "#   #",
+                "#### ",
+                "#    ",
+                "#    ");
         Tile tile = new Tile(list);
-        System.out.println(tile.orientationsAsString());
-        assertTrue(true);
+        final var orientationsAsString = tile.orientationsAsString();
+        System.out.println(orientationsAsString);
+
+        final var oriCount = list.joinToStringX().count(c -> c == '#');
+        final var resultCount = StringX.of(orientationsAsString).count(c -> c == '#');
+        assertEquals(oriCount * 8, resultCount);
     }
 
 }
